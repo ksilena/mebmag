305fe4425ff44bb89f720b78e027149f6e23d448
Toxicoza <ksilena@gmail.com>
2012-09-01 14:12:07 +0400

    add comments

--- belarus.py
+++ belarus.py
@@ -5,23 +5,31 @@ from soupselect import select
 from urllib2 import urlopen
 import re
 
-kill_tag = re.compile('<[^<]*>')
-sub_quot = re.compile('&laquo; *| *&raquo;|&quot;')
-kill_space = re.compile('&nbsp;| +|\t+')
-kill_str = re.compile('\n|\r')
+################ regular expression ####################
+kill_tag = re.compile('<[^<]*>') #поиск тэга
+sub_quot = re.compile('&laquo; *| *&raquo;|&quot;') #поиск кавычек
+kill_space = re.compile('&nbsp;| +|\t+') #поиск пробел и табуляции
+kill_str = re.compile('\n|\r') #поиск конца строки
+###########################################################
 
+##################### function for cleaning str ############
 def clean_str(st):
     if st: st = kill_tag.sub('', str(st))
     if st: st = sub_quot.sub('"', st)
     if st: st = kill_space.sub(" ", st)
     if st: st = kill_str.sub('', st)
     return st.strip()
+#############################################################
+
+# set initial variables #
 url = "http://mebelbelarusi.ru"
 doc = soup(urlopen(url))
 counter = 0
 c_2 = 0
 div_block = select(doc, "ul.left_menu a")
-for a in select(doc, "ul.left_menu a"):
+
+
+for a in div_block:
     counter += 1
     li = soup(urlopen(url + a["href"]))
     name =  select(li, "a.p_name")
@@ -33,4 +41,4 @@ for a in select(doc, "ul.left_menu a"):
             h3 = select(i_name, "h3")
             print "Название:", clean_str(h3[0])
             print clean_str(mat[0])
-            print img
+            print url + img[0]["href"]
